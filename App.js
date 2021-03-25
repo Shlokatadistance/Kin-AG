38c9ee035457da6a8ed3832f54b7ce4d7942a558
Shlokatadistance <47189916+Shlokatadistance@users.noreply.github.com>
2021-03-25 11:35:58 +0530

    Files

--- App.js
+++ App.js
@@ -6,21 +6,15 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import RBSheet from "react-native-raw-bottom-sheet";
 import Cf from './cfg.js';
-import Client1 from './StartupPage.js';
-import Client2 from './ClientsPage.js';
-import Client3 from './ClientProfile.js';
-import Client4 from './ClientScheduling.js';
-import Client5 from './ClientDetails.js';
-import Client6 from './ClientDetails2.js';
-import Client7 from './ClientDetails3.js';
-import Client8 from './ClientDetails4.js';
-import Client9 from './ClientDetails5.js';
+import Client1 from './Old/Navigation.js';
+
+
 
 
 export default function App() {
   return (
 
-     <Client2/>
+     <Client1/>
 
 
   );
